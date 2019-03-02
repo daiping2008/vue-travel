88fb5733ddf059f897e7a431ce97021fb12f9007
daiping2008 <1170174401@qq.com>
2019-03-02 12:26:58 +0800

    '添加详情页Banner功能实现和画廊组件'

---
+++ src/views/detail/components/banner.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="banner" @click="handleShowGallary">
+      <img class="banner-img" :src="bannerImg" />
+      <div class="banner-info">
+        <div class="banner-tittle">
+          {{sightName}}
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe696;</span>
+          {{gallaryImgs.length}}
+        </div>
+      </div>
+    </div>
+    <gallary :imgs="gallaryImgs" @close="close" v-show="showGallary"></gallary>
+  </div>
+</template>
+
+<script>
+import Gallary from '@/components/gallary'
+export default {
+  props: ['bannerImg', 'sightName', 'gallaryImgs'],
+  data () {
+    return {
+      showGallary: false
+    }
+  },
+  methods: {
+    handleShowGallary () {
+      this.showGallary = true
+    },
+    close () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    Gallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-tittle
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
