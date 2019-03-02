dd42bd1295b34423b48ac2500db90679c4eb881c
daiping2008 <1170174401@qq.com>
2019-03-02 15:34:47 +0800

    '真机测试解决2个bug问题，一个是城市选择无法单击，一个是scroppTop值为0'

--- src/views/city/components/search.vue
+++ src/views/city/components/search.vue
@@ -36,7 +36,9 @@ export default {
     }
   },
   mounted () {
-    this.scroll = new BScroll(this.$refs.search)
+    this.scroll = new BScroll(this.$refs.search, {
+      click: true
+    })
   },
   computed: {
     hasNoData () {
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 // vue.config.js
 module.exports = {
   devServer: {
+    host: '0.0.0.0',
+    port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'http://192.168.1.117:3000/',
         changeOrigin: true,
         ws: true
       }
